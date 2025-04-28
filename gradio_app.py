27c50e4cb4c8a7822f993aa537294a7498e0859f
JeremyCCHsu <jeremycchsu@gmail.com>
2025-04-28 03:12:11 +0000

    add pace & temperature; rename emotion_adv to exaggeration in the interface

--- gradio_app.py
+++ gradio_app.py
@@ -3,16 +3,24 @@ import gradio as gr
 
 
 model = OratorTTS.from_local("checkpoints", "cuda")
-def generate(text, audio_prompt_path, emotion_adv):
-    wav = model.generate(text, audio_prompt_path=audio_prompt_path, emotion_adv=emotion_adv)
+def generate(text, audio_prompt_path, exaggeration, pace, temperature):
+    wav = model.generate(
+        text, audio_prompt_path=audio_prompt_path,
+        exaggeration=exaggeration,
+        pace=pace,
+        temperature=temperature,
+    )
     return 24000, wav.squeeze(0).numpy()
+
 
 demo = gr.Interface(
     generate,
     [
         gr.Textbox(value="What does the fox say?", label="Text to synthesize"),
-        gr.Audio(sources="upload", type="filepath", label="Input Audio File"),
-        gr.Slider(0, 1, step=.05, label="emotion_adv", value=.5),
+        gr.Audio(sources="upload", type="filepath", label="Input Audio File", value=None),
+        gr.Slider(-1, 2, step=.05, label="exaggeration", value=.5),
+        gr.Slider(0.8, 1.2, step=.01, label="pace", value=1),
+        gr.Slider(0.05, 5, step=.05, label="temperature", value=.8),
     ],
     "audio",
 )
