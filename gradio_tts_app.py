770f2cd28990c0f5a408cf8f44a6aa067fa5b1ee
Jeremy Hsu <jeremycchsu@gmail.com>
2025-05-16 19:36:23 +0800

    hide seed/temp/pace by default (#14)

--- gradio_tts_app.py
+++ gradio_tts_app.py
@@ -1,29 +1,66 @@
-from chatterbox.tts import ChatterboxTTS
+import random
+import numpy as np
+import torch
 import gradio as gr
+from chatterbox.tts import ChatterboxTTS
 
 
-model = ChatterboxTTS.from_local("checkpoints", "cuda")
-def generate(text, audio_prompt_path, exaggeration, pace, temperature):
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def set_seed(seed: int):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+model = ChatterboxTTS.from_pretrained(DEVICE)
+
+def generate(text, audio_prompt_path, exaggeration, pace, temperature, seed_num):
+    if seed_num != 0:
+        set_seed(int(seed_num))
+
     wav = model.generate(
-        text, audio_prompt_path=audio_prompt_path,
+        text,
+        audio_prompt_path=audio_prompt_path,
         exaggeration=exaggeration,
         pace=pace,
         temperature=temperature,
     )
-    return 24000, wav.squeeze(0).numpy()
+    return model.sr, wav.squeeze(0).numpy()
 
 
-demo = gr.Interface(
-    generate,
-    [
-        gr.Textbox(value="What does the fox say?", label="Text to synthesize"),
-        gr.Audio(sources="upload", type="filepath", label="Reference Audio File", value=None),
-        gr.Slider(-5, 5, step=.05, label="exaggeration", value=.5),
-        gr.Slider(0.8, 1.2, step=.01, label="pace", value=1),
-        gr.Slider(0.05, 5, step=.05, label="temperature", value=.8),
-    ],
-    "audio",
-)
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            text = gr.Textbox(value="What does the fox say?", label="Text to synthesize")
+            ref_wav = gr.Audio(sources="upload", type="filepath", label="Reference Audio File", value=None)
+            exaggeration = gr.Slider(-5, 5, step=.05, label="exaggeration", value=.5)
+
+            with gr.Accordion("More options", open=False):
+                seed_num = gr.Number(value=0, label="Random seed (0 for random)")
+                temp = gr.Slider(0.05, 5, step=.05, label="temperature", value=.8)
+                pace = gr.Slider(0.8, 1.2, step=.01, label="pace", value=1)
+
+            run_btn = gr.Button("Generate", variant="primary")
+
+        with gr.Column():
+            audio_output = gr.Audio(label="Output Audio")
+
+    run_btn.click(
+        fn=generate,
+        inputs=[
+            text,
+            ref_wav,
+            exaggeration,
+            pace,
+            temp,
+            seed_num,
+        ],
+        outputs=audio_output,
+    )
 
 if __name__ == "__main__":
     demo.launch()
