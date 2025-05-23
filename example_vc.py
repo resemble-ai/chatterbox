d684479db844484a613d83f64a3532d2cb548232
JeremyCCHsu <jeremycchsu@gmail.com>
2025-05-23 06:45:24 +0000

    fix a box in example_vc.py

--- example_vc.py
+++ example_vc.py
@@ -1,6 +1,6 @@
 from chatterbox.vc import ChatterboxVC
 
-model = ChatterboxVC.from_local("checkpoints", "cuda")
+model = ChatterboxVC.from_pretrained("cuda")
 wav = model.generate("tests/trimmed_8b7f38b1.wav")
 import torchaudio as ta
 ta.save("testvc.wav", wav, model.sr)
