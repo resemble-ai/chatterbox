eaab947f473c2c7b1f3d87dd8164eb4653da263c
Chintella-Esq <bbc@chintellalaw.com>
2025-12-25 12:44:21 -0500

    Add files via upload

--- src/chatterbox/models/s3tokenizer/s3tokenizer.py
+++ src/chatterbox/models/s3tokenizer/s3tokenizer.py
@@ -1,9 +1,8 @@
 from typing import List, Tuple
 
 import numpy as np
-import torchaudio as ta
-_fb = ta.functional.create_fb_matrix if hasattr(ta.functional, "create_fb_matrix") else ta.functional.melscale_fbanks
 import torch
+import torchaudio as ta
 import torch.nn.functional as F
 from s3tokenizer.utils import padding
 from s3tokenizer.model_v2 import (
@@ -11,22 +10,16 @@ from s3tokenizer.model_v2 import (
     ModelConfig,
 )
 
+_fb = ta.functional.melscale_fbanks if hasattr(ta.functional, "melscale_fbanks") else ta.functional.create_fb_matrix
 
-# Sampling rate of the inputs to S3TokenizerV2
 S3_SR = 16_000
-S3_HOP = 160  # 100 frames/sec
-S3_TOKEN_HOP = 640  # 25 tokens/sec
+S3_HOP = 160
+S3_TOKEN_HOP = 640
 S3_TOKEN_RATE = 25
 SPEECH_VOCAB_SIZE = 6561
 
 
 class S3Tokenizer(S3TokenizerV2):
-    """
-    s3tokenizer.S3TokenizerV2 with the following changes:
-    - a more integrated `forward`
-    - compute `log_mel_spectrogram` using `_mel_filters` and `window` in `register_buffers`
-    """
-
     ignore_state_dict_missing = ("_mel_filters", "window")
 
     def __init__(
@@ -43,10 +36,10 @@ class S3Tokenizer(S3TokenizerV2):
             sample_rate=S3_SR,
             f_min=0,
             f_max=S3_SR // 2,
-        ).T  # torchaudio returns (n_freqs, n_mels)
+        ).T
         self.register_buffer(
             "_mel_filters",
-            torch.FloatTensor(_mel_filters),
+            _mel_filters.float(),
         )
 
         self.register_buffer(
@@ -55,9 +48,6 @@ class S3Tokenizer(S3TokenizerV2):
         )
 
     def pad(self, wavs, sr) -> List[torch.Tensor]:
-        """
-        Given a list of wavs with the same `sample_rate`, pad them so that the length is multiple of 40ms (S3 runs at 25 token/sec).
-        """
         processed_wavs = []
         for wav in wavs:
             if isinstance(wav, np.ndarray):
@@ -79,7 +69,6 @@ class S3Tokenizer(S3TokenizerV2):
         return processed_wavs
 
     def _prepare_audio(self, wavs):
-        """Prepare a list of audios for s3tokenizer processing."""
         processed_wavs = []
         for wav in wavs:
             if isinstance(wav, np.ndarray):
@@ -97,23 +86,13 @@ class S3Tokenizer(S3TokenizerV2):
         accelerator: 'Accelerator'=None,
         max_len: int=None,
     ) -> Tuple[torch.Tensor, torch.LongTensor]:
-        """
-        NOTE: mel-spec has a hop size of 160 points (100 frame/sec).
-        FIXME: this class inherits `nn.Module` but doesn't accept `torch.Tensor` and handles a list of wavs one by one, which is unexpected.
-
-        Args
-        ----
-        - `wavs`: 16 kHz speech audio
-        - `max_len` max length to truncate the output sequence to (25 token/sec).
-        NOTE: please pad the waveform if longer sequence is needed.
-        """
         processed_wavs = self._prepare_audio(wavs)
         mels, mel_lens = [], []
         for wav in processed_wavs:
             wav = wav.to(self.device)
-            mel = self.log_mel_spectrogram(wav)  # [B=1, F, T]
+            mel = self.log_mel_spectrogram(wav)
             if max_len is not None:
-                mel = mel[..., :max_len * 4]  # num_mel_frames = 4 * num_tokens
+                mel = mel[..., :max_len * 4]
             mels.append(mel.squeeze(0))
 
         mels, mel_lens = padding(mels)
@@ -133,23 +112,6 @@ class S3Tokenizer(S3TokenizerV2):
         audio: torch.Tensor,
         padding: int = 0,
     ):
-        """
-        Compute the log-Mel spectrogram of
-
-        Parameters
-        ----------
-        audio: torch.Tensor, shape = (*)
-            The path to audio or either a NumPy array or Tensor containing the
-            audio waveform in 16 kHz
-
-        padding: int
-            Number of zero samples to pad to the right
-
-        Returns
-        -------
-        torch.Tensor, shape = (128, n_frames)
-            A Tensor that contains the Mel spectrogram
-        """
         if not torch.is_tensor(audio):
             audio = torch.from_numpy(audio)
 
